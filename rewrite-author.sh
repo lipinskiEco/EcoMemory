eae6429444822b1810d658f047331240ce48c7a0
Artur Lipinski <188866942+lipinskiEco@users.noreply.github.com>
2026-06-16 20:41:40 +0500

    feat: pre-fill USDC and improve deploy page UX

---
+++ rewrite-author.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export GIT_AUTHOR_NAME="Artur Lipinski"
+export GIT_AUTHOR_EMAIL="188866942+lipinskiEco@users.noreply.github.com"
+export GIT_COMMITTER_NAME="Artur Lipinski"
+export GIT_COMMITTER_EMAIL="188866942+lipinskiEco@users.noreply.github.com"
