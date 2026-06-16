ba50d1ffeadea73f330c299fa3ac1cb0f17f2495
Artur Lipinski <188866942+lipinskiEco@users.noreply.github.com>
2026-06-16 20:08:50 +0100

    chore: remove temporary rewrite script

--- rewrite-author.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export GIT_AUTHOR_NAME="Artur Lipinski"
-export GIT_AUTHOR_EMAIL="188866942+lipinskiEco@users.noreply.github.com"
-export GIT_COMMITTER_NAME="Artur Lipinski"
-export GIT_COMMITTER_EMAIL="188866942+lipinskiEco@users.noreply.github.com"
